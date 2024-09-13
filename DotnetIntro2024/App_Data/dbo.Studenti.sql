f90e27b998d333a19e526455836ff204f43cbe6d
ProfOscar <oscar.cambieri@vallauri.edu>
2024-09-13 13:23:37 +0200

    Sample DB added

---
+++ DotnetIntro2024/App_Data/dbo.Studenti.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Studenti]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(50) NULL, 
+    [Cognome] VARCHAR(50) NULL, 
+    [Classe] INT NULL, 
+    [AnnoNascita] SMALLINT NULL, 
+    [Genere] CHAR(1) NULL
+)
